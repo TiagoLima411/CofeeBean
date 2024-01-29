d0eca12349fa7f747fb9d251e3e8700a4f660289
Tiago Alves <tiago_lima411@hotmail.com>
2024-01-29 14:32:10 -0300

    remove hardcoded token

--- app/services/ip_service/ip_info.rb
+++ app/services/ip_service/ip_info.rb
@@ -7,13 +7,14 @@ module IpService
   # app/services/ip_service/ip_info.rb
   class IpInfo
     BASE_URL = 'https://ipinfo.io'
+    TOKEN = Rails.application.credentials.IP_INFO_TOKEN
 
     def initialize(ip)
       @ip = ip
     end
 
     def info
-      uri = URI("#{BASE_URL}/#{@ip}?token=1624919ce2bea9")
+      uri = URI("#{BASE_URL}/#{@ip}?token=#{TOKEN}")
       response = Net::HTTP.get(uri)
       JSON.parse(response)
     end
