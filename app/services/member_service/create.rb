f14ede6230aba4671e20d2282f9ae63748a7d61d
Tiago Alves <tiago_lima411@hotmail.com>
2024-01-29 10:53:38 -0300

    add logs

--- app/services/member_service/create.rb
+++ app/services/member_service/create.rb
@@ -11,20 +11,20 @@ module MemberService
     attr_accessor :member
     attr_reader :list, :errors
 
-    def initialize(params)
-      @service_read = MemberService::Read.new
-      @list = @service_read.list
-      @params = params
-      @member = nil
+    def initialize(member)
+      @service = MemberService::Read.new
+      @list = @service.list
+      @member = member
       @errors = []
     end
 
     def create
       run_callbacks :create do
-        @member.id = @list.length + 1
+        @member.id = list.nil? ? 1 : @list.length + 1
         insert(@member)
         find(@member.id)
 
+        log("MEMBER CREATED -- ID: #{@member.id} -- NAME: #{@member.name} -- EMAIL: #{@member.email}".upcase)
         true
       end
     end
@@ -32,22 +32,28 @@ module MemberService
     private
 
     def validate_member
-      @member = Member.new(@params)
       return if @member.valid?
 
       @member.errors.each do |e|
         errors << "#{e.attribute} #{e.message}"
+        log("VALIDATION ERROR -- #{e.attribute} #{e.message}".upcase, level: :error)
       end
 
       throw :abort
     end
 
     def insert(member)
-      @list << member
+      @service.initialize_members if @list.nil?
+      @service.list << member
+      log("INSERT NEW MEMBER TOTAL -- #{@service.list.count}")
     end
 
     def find(id)
-      @service_read.find(id)
+      @service.find(id)
+    end
+
+    def log(message, level: :info)
+      Rails.logger.tagged(self.class.name) { Rails.logger.send(level, message) }
     end
   end
 end
